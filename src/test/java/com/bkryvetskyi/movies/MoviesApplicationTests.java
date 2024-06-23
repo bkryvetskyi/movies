e15c8184be772d078ac9dafbad3595ab92171ca5
Bohdan Kryvetskyy <bkryvetskyi@gmail.com>
2024-06-23 16:52:10 +0300

    Initial commit.

---
+++ src/test/java/com/bkryvetskyi/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkryvetskyi.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
